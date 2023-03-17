66ec411581a24063583453417937f2c66dff414b
asoftwareworld <asoftwareworld@gmail.com>
2023-03-17 10:35:24 +0530

    fixed demo issue

--- projects/qr-code-demo/src/app/app.component.ts
+++ projects/qr-code-demo/src/app/app.component.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright ASW (A Software World) All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file
+ */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MiddleShape, CornerInnerShape, CornerOuterShape, LogoStyle, Density } from '@asoftwareworld/qrcode';
